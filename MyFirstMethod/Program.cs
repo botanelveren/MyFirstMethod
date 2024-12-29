13da6b002de8560f606b2b79b1751521c15b68fc
Botan Elveren <botanelveren@gmail.com>
2024-12-29 16:39:52 +0300

    Add project files.

---
+++ MyFirstMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyFirstMehod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            void Lyrics()
+
+            {
+                Console.WriteLine("Show must go on!");
+            }
+
+            int RandomNumber()
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 10);
+                return number % 2;
+            }
+
+            int ProductAxB(int a, int b)
+            {
+
+                return a * b;
+            }
+
+            void Hello(string name, string surname)
+            {
+                Console.WriteLine($"Hoş geldiniz {name} {surname}");
+            }
+            ///////////////////////////////////////////////////////////////////////
+
+            Lyrics();
+
+            int remainder = RandomNumber();
+            Console.WriteLine($"Sayının ikiye bölümünden kalan: {remainder}");
+
+            int product = ProductAxB(6, 8);
+            Console.WriteLine($"Sayıların çapımı: {product}");
+
+            Hello("Botan", "Elveren");
+
+        }
+
+    }
+}
